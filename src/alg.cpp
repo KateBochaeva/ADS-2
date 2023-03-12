3435d02e12a5f65999511ab2b5057975eee3ce7f
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-03-12 21:22:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -37,14 +37,14 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double sum=0;
-  for (int i=1; i<count/2; i++)
+  for (int i=1; i<=count/2; i++)
     sum+=calcItem(x,i*2-1);
   return sum;
 }
 
 double cosn(double x, uint16_t count) {
   double sum=0;
-  for (int i=0; i<count/2; i++)
+  for (int i=0; i<=count/2; i++)
     sum+=calcItem(x,i*2);
   return sum;
 }
